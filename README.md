5b3a59a858774f9f14c52a972189a60a971590cb
NaveenReddy0427 <naveendeva909@gmail.com>
2024-07-19 08:43:35 +0530

    created ecommerce store

---
+++ README.md
@@ -0,0 +1,37 @@
+# Ecommerce Store
+
+## Project Description
+
+Ecommerce Store is a web application that allows users to manage products and make purchases. Users can add product to the cart and edit and delete the product if want and can checkout the product through a simple and intuitive interface and the admin can add, delete, edit both products and users who have registered.
+
+## Features
+
+- Add Product
+- Edit Product
+- Delete Product
+- Buy Product
+
+## Technologies Used
+
+- React.Js
+- JavaScript
+- Mongoose
+- MongoDB
+- Node.Js
+- CSS (Tailwind)
+- Express.Js
+- RazorPay (payment gateway service)
+
+## Prerequisites
+
+Ensure you have the following installed on your system:
+
+- Node.js (version 14 or higher)
+- MongoDB
+
+## Installation Instructions
+
+1. Clone the repository:
+
+   ```bash
+   git clone 
---
+++ frontend/src/main.jsx
@@ -0,0 +1,52 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/features/store.js";
+
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Profile from "./pages/User/Profile.jsx";
+
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
+import ProductList from "./pages/Admin/ProductList.jsx";
+import ProductUpdate from "./pages/Admin/ProductUpdate.jsx";
+import AllProducts from "./pages/Admin/AllProducts.jsx";
+
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+
+      
+      <Route path="/admin" element={<AdminRoute />} >
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />} />
+        <Route path="productlist/:pageNumber" element={<ProductList />} />
+        <Route path="product/update/:_id" element={<ProductUpdate />} />
+      </Route>
+
+    </Route>
+  )
+)
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
+);
